cc94c1bd947e71c474c053608aa95e37fcc1d5d1
raiyan <rsahmed1290@gmail.com>
2022-12-15 01:12:49 -0800

    added formatted strings test

--- cpp-transpiler/testsrc/lexer_test.cpp
+++ cpp-transpiler/testsrc/lexer_test.cpp
@@ -1,7 +1,7 @@
 #include <gtest/gtest.h>
 #include "lexer.h"
 
-TEST(LexerTest, SimpleExample) {
+TEST(LexerTest, Simple) {
     
     std::vector<Token> expectedTokens;
 
@@ -25,9 +25,9 @@ TEST(LexerTest, SimpleExample) {
 
         for(int i = 0; i < tokens.size(); i++) {
             EXPECT_EQ(tokens[i].data, expectedTokens[i].data) << 
-                " \u001b[1m\u001b[31mFailure at index " << i << std::endl;
+                " \u001b[1m\u001b[31mFailure at index " << i << "\u001b[0m" << std::endl;
             EXPECT_EQ(tokens[i].type, expectedTokens[i].type) << 
-                " \u001b[1m\u001b[31mFailure at index " << i << std::endl;
+                " \u001b[1m\u001b[31mFailure at index " << i << "\u001b[0m" << std::endl;
         }
     }
 
@@ -52,7 +52,61 @@ TEST(LexerTest, SimpleExample) {
             EXPECT_EQ(tokens[i].data, expectedTokens[i].data) << 
                 "\u001b[1m\u001b[31mFailure at index " << i << "\u001b[0m" << std::endl;
             EXPECT_EQ(tokens[i].type, expectedTokens[i].type) << 
-                " \u001b[1m\u001b[31mFailure at index " << i << std::endl;
+                " \u001b[1m\u001b[31mFailure at index " << i << "\u001b[0m" << std::endl;
+        }
+    }
+
+}
+
+TEST(LexerTest, FormatString) {
+    
+    std::vector<Token> expectedTokens;
+
+    {
+        std::string fileData = 
+            "stdout!(\"Hello ${p1.name}, how is ${p2.name} doing?\");\n"
+            "stdout!(\"\\${escaped}\");";
+
+        expectedTokens = {
+            Token("stdout", TokenType::Identifier), 
+            Token("!", TokenType::SpecialSymbol), 
+            Token("(", TokenType::SpecialSymbol), 
+            Token("\"Hello ", TokenType::StringSegment),
+            Token("$", TokenType::SpecialSymbol),
+            Token("{", TokenType::SpecialSymbol),
+            Token("p1", TokenType::Identifier),
+            Token(".", TokenType::SpecialSymbol),
+            Token("name", TokenType::Identifier),
+            Token("}", TokenType::SpecialSymbol),
+            Token(", how is ", TokenType::StringSegment),
+            Token("$", TokenType::SpecialSymbol),
+            Token("{", TokenType::SpecialSymbol),
+            Token("p2", TokenType::Identifier),
+            Token(".", TokenType::SpecialSymbol),
+            Token("name", TokenType::Identifier),
+            Token("}", TokenType::SpecialSymbol),
+            Token(" doing?\"", TokenType::StringSegment),
+            Token(")", TokenType::SpecialSymbol),
+            Token(";", TokenType::SpecialSymbol),
+
+            Token("stdout", TokenType::Identifier), 
+            Token("!", TokenType::SpecialSymbol), 
+            Token("(", TokenType::SpecialSymbol),
+            Token("\"\\${escaped}\"", TokenType::String),
+            Token(")", TokenType::SpecialSymbol),
+            Token(";", TokenType::SpecialSymbol),
+        };
+        
+        std::vector<Token> tokens;
+        Lexer::Lex(fileData, tokens);
+        
+        ASSERT_EQ(tokens.size(), expectedTokens.size());
+
+        for(int i = 0; i < tokens.size(); i++) {
+            EXPECT_EQ(tokens[i].data, expectedTokens[i].data) << 
+                " \u001b[1m\u001b[31mFailure at index " << i << "\u001b[0m" << std::endl;
+            EXPECT_EQ(tokens[i].type, expectedTokens[i].type) << 
+                " \u001b[1m\u001b[31mFailure at index " << i << "\u001b[0m" << std::endl;
         }
     }
 
