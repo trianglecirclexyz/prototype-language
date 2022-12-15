85b8039c4ad5c439ccc72ab002f248cd46dbea80
raiyan <rsahmed1290@gmail.com>
2022-12-15 01:13:10 -0800

    Lex() bool to void

--- cpp-transpiler/inc/lexer.h
+++ cpp-transpiler/inc/lexer.h
@@ -38,12 +38,13 @@ namespace Lexer {
     const std::unordered_set<std::string> operators = 
         {"=", "+"};
     const std::unordered_set<char> specialSymbols = 
-        {'(', ')', '{', '}', ';', '!', '.', '$',};
+        {'(', ')', '{', '}', ';', '!', '.', '$',
+         ',', ':', };
 
     // read file into fileData
     bool ReadFile(const std::string &filePath, std::string &fileData);
     // lex file into tokens 
-    bool Lex(const std::string &fileData, std::vector<Token> &tokens);
+    void Lex(const std::string &fileData, std::vector<Token> &tokens);
 };
 
 #endif // LEXER_H
